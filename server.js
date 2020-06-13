adcd324b1971dc85ff4155a481cc36586ae81ead
unknown <patelhet32@gmail.com>
2020-06-13 19:21:31 -0400

    new

--- server.js
+++ server.js
@@ -99,6 +99,7 @@ app.get("/download", (req, res) => {
   var URL = req.query.URL;
   var name = req.query.name;
 
+  res.setHeader("Content-Type", "audio/mpeg");
   res.header("Content-Disposition", 'attachment; filename="' + name + '.mp3"');
 
   ytdl(URL, {
