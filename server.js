d00501d4727267aaf4136ab3e38065fdbbc625c4
Het Patel <patelhet32@gmail.com>
2020-06-19 14:51:39 -0400

    er

--- server.js
+++ server.js
@@ -13,7 +13,6 @@ app.use(cors());
 app.get("/getplaylist", (req, res) => {
   var pid = req.query.pid;
 
-  if (pid.length != 22) res.send({ failed: "no such playlist" });
 
   var TOTAL = 0;
   var rtotal = 0;
